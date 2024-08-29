5bddc52b5ecc2946df6071aebb714dc0de272f9e
João <mcruznq@gmail.com>
2024-08-29 20:52:52 -0300

    dishcard favorite dish function

--- src/components/dish-card/index.jsx
+++ src/components/dish-card/index.jsx
@@ -3,7 +3,7 @@ import { USER_ROLE } from "../../utils/user-roles";
 
 import { Container } from "./styles";
 
-import { PiHeartStraight, PiPencilSimpleLight  } from "react-icons/pi";
+import { PiHeartStraight, PiPencilSimpleLight, PiHeartStraightFill } from "react-icons/pi";
 
 import { Button } from "../button";
 import { Stepper } from "../stepper";
@@ -12,28 +12,44 @@ import dishPlaceholder from "/placeholder.png";
 
 import { api } from "../../services/api.js";
 
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export function DishCard({data}){
   const { user } = useAuth();
 
+  const [isFavorited, setIsFavorited] = useState(false);
+
   const dishURL = data.photo ? `${api.defaults.baseURL}/files/${data.photo}` : dishPlaceholder;
 
   const navigate = useNavigate();
 
+  function handleFavorites(){
+    setIsFavorited(prev => !prev)
+  };
+
   function handleDetails(){
     navigate(`/details/${data.id}`)
-  }
+  };
 
   function handleEdit(){
     navigate(`/edit/${data.id}`)
-  }
+  };
 
   return (
     <Container>
 
-      {[USER_ROLE.ADMIN].includes(user.role) && <PiPencilSimpleLight size={28} onClick={handleEdit}/>}
-      {[USER_ROLE.CUSTOMER].includes(user.role) && <PiHeartStraight size={24}/>}
+      {[USER_ROLE.ADMIN].includes(user.role) &&
+        <PiPencilSimpleLight size={28} onClick={handleEdit}/>
+      }
+
+      {[USER_ROLE.CUSTOMER].includes(user.role) &&
+        <PiHeartStraight size={24} onClick={handleFavorites}/>
+      }
+
+      {isFavorited && (
+        <PiHeartStraightFill size={24} onClick={handleFavorites}/>
+      )}
 
       <section className="main-info">
         <img src={dishURL} alt={data.name} onClick={handleDetails}/>

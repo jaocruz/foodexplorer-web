da4ba0a2d32e26d284e283120d02bf696a885309
João <mcruznq@gmail.com>
2024-08-29 21:03:29 -0300

    stepper counter function

--- src/components/stepper/index.jsx
+++ src/components/stepper/index.jsx
@@ -2,12 +2,24 @@ import { Container } from "./styles";
 
 import { PiPlus, PiMinus } from "react-icons/pi";
 
+import { useState } from "react";
+
 export function Stepper(){
+  const [count, setCount] = useState(1);
+
+  function increment(){
+    setCount(prevCount => Math.min(9, prevCount + 1));
+  };
+
+  function decrement(){
+    setCount(prevCount => Math.max(1, prevCount - 1));
+  }
+
   return (
     <Container>
-      <a href="#"><PiMinus size={24}/></a>
-      <h1>01</h1>
-      <a href="#"><PiPlus size={24}/></a>
+      <a onClick={decrement}><PiMinus size={24}/></a>
+      <h1>0{count}</h1>
+      <a onClick={increment}><PiPlus size={24}/></a>
     </Container>
   )
 }

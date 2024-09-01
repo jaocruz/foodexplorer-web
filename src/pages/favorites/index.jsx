c49ed20957ab81967be2eb07eebfa24582f60a49
João <mcruznq@gmail.com>
2024-09-01 15:06:01 -0300

    favorites map

--- src/pages/favorites/index.jsx
+++ src/pages/favorites/index.jsx
@@ -3,7 +3,29 @@ import { Container } from "./styles";
 import { Header } from "../../components/header"
 import { Footer } from "../../components/footer"
 
+import { api } from "../../services/api";
+import { useEffect, useState } from "react";
+
 export function Favorites(){
+  const [favorite, setFavorite] = useState([]);
+
+  const dishURL = `${api.defaults.baseURL}/files/`;
+
+  useEffect(() => {
+    async function loadFavorites() {
+      try {
+        const response = await api.get("/favorites");
+        setFavorite(response.data);
+      }
+
+      catch(error) {
+        console.error("Não foi possivel carregar favoritos:", error);
+      }
+    }
+
+    loadFavorites();
+  }, []);
+
   return(
     <>
     <Header/>
@@ -12,77 +34,18 @@ export function Favorites(){
       <h1>Meus favoritos</h1>
 
       <section>
-        <div className="favorite-dishes">
-          <img src="/macarons.png" alt="" />
+        {
+          favorite.map((favorite, index) => (
+            <div className="favorite-dishes" key={index}>
+              <img src={`${dishURL}${favorite.photo}`}/>
 
-          <div className="dish-info">
-            <h1>Macarons</h1>
-            <a>Remover dos favoritos</a>
-          </div>
-        </div>
-
-        <div className="favorite-dishes">
-          <img src="/macarons.png" alt="" />
-
-          <div className="dish-info">
-            <h1>Macarons</h1>
-            <a>Remover dos favoritos</a>
-          </div>
-        </div>
-
-        <div className="favorite-dishes">
-          <img src="/macarons.png" alt="" />
-
-          <div className="dish-info">
-            <h1>Macarons</h1>
-            <a>Remover dos favoritos</a>
-          </div>
-        </div>
-
-        <div className="favorite-dishes">
-          <img src="/macarons.png" alt="" />
-
-          <div className="dish-info">
-            <h1>Macarons</h1>
-            <a>Remover dos favoritos</a>
-          </div>
-        </div>
-
-        <div className="favorite-dishes">
-          <img src="/macarons.png" alt="" />
-
-          <div className="dish-info">
-            <h1>Macarons</h1>
-            <a>Remover dos favoritos</a>
-          </div>
-        </div>
-
-        <div className="favorite-dishes">
-          <img src="/macarons.png" alt="" />
-
-          <div className="dish-info">
-            <h1>Macarons</h1>
-            <a>Remover dos favoritos</a>
-          </div>
-        </div>
-        
-        <div className="favorite-dishes">
-          <img src="/macarons.png" alt="" />
-
-          <div className="dish-info">
-            <h1>Macarons</h1>
-            <a>Remover dos favoritos</a>
-          </div>
-        </div>
-
-        <div className="favorite-dishes">
-          <img src="/macarons.png" alt="" />
-
-          <div className="dish-info">
-            <h1>Macarons</h1>
-            <a>Remover dos favoritos</a>
-          </div>
-        </div>
+              <div className="dish-info">
+                <h1>{favorite.name}</h1>
+                <a>Remover dos favoritos</a>
+              </div>
+            </div>
+          ))
+        }
       </section>
     </Container>
 
